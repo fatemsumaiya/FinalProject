73410d920031cf9b557d5dc6bd950fe4b5733cc7
fatemsumaiya <fatemsumaiya@gmail.com>
2024-12-21 17:12:45 -0500

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,5 @@ const app = express();
 
 //callback function
 app.listen(PORT, () => {
-    console.log('App is listening to port: ${PORT}')
+    console.log(`App is listening to port: ${PORT}`); // Use backticks for string interpolation
 });
