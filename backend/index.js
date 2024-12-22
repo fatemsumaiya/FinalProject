4a2cdca0f2e650dd3fc9aa08b47c9cd69f9bdc26
fatemsumaiya <fatemsumaiya@gmail.com>
2024-12-21 19:43:04 -0500

    Created a book object and successfully saved it in database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import {Book} from './models/bookModel.js'
 
 const app = express();
+
+app.use(express.json());
 
 //here we create our new https server to connect to our server
 app.get('/', (request, response) => {
@@ -10,8 +13,29 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to MERN Stack Turtorial')
 });
 
+//route for saving a new book
+app.post('/books', async(request, response) => {
+    try{ 
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear 
+        ){}
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+    //our variable for new book
+    const newBook = {
+        title: request.body.title,
+        author: request.body.author,
+        publishYear: request.body.publishYear,
+    };
 
+    const book = await Book.create(newBook);
 
+    return response.status(201).send(book);
+});
 
 mongoose
     .connect(mongoDBURL)
