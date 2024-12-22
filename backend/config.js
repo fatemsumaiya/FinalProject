de8ba741926502b7e2b0e835309abaf118aeba89
fatemsumaiya <fatemsumaiya@gmail.com>
2024-12-21 19:08:51 -0500

    Added a mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://fatemsumaiya:cb1AI7gxGexQdrUy@books-store-mern.xfhoy.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-//callback function
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`); // Use backticks for string interpolation
+//here we create our new https server to connect to our server
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to MERN Stack Turtorial')
 });
+
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`); // Use backticks for string interpolation
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
