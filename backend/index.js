1265c6a3e7e872f62d4d62df7194e3abab31fb07
fatemsumaiya <fatemsumaiya@gmail.com>
2024-12-21 20:10:52 -0500

    Created a UpdateBook function

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,57 @@ app.post('/books', async(request, response) => {
     return response.status(201).send(book);
 });
 
+//our route to get All books from database
+app.get('/books/:id', async(request, response) => {
+    try{
+
+        const{ id } = request.params;
+
+       const book= await Book.findById(id); 
+
+
+       return response.status(200).json({book});
+    }catch(error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route to update a book
+app.put('/books/:id', async (request, response) => {
+    try {
+      if( 
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear 
+      ) {
+        return response.status(400).send({
+            message: 'Send all required fields: title, author, publishYear'
+        });
+      }
+
+      const{id} = request.params; 
+
+      const result = await Book.findByIdAndUpdate(id, request.body);
+
+      if(!result){
+        return response.status(404).json({message: 'Book not found'});
+      }
+
+      return response.status(200).send({message: 'Book update successfully'});
+
+
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    
+    
+    }
+}
+
+
+);
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
